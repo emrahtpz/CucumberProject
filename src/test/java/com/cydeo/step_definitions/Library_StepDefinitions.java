a0df5b3fa06be82815f11674a47d26aec8798244
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-20 21:35:44 +0300

    Working on the Library app

---
+++ src/test/java/com/cydeo/step_definitions/Library_StepDefinitions.java
@@ -0,0 +1,27 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Library_StepDefinitions {
+
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        System.out.println("User is on the login page");
+    }
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        System.out.println("User enters librarian username");
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        System.out.println("User enters librarian password");
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        System.out.println("User sees the library app dashboard");
+    }
+
+}
--- src/test/resources/features/googleSearch.feature
+++ src/test/resources/features/googleSearch.feature
@@ -6,7 +6,7 @@ Feature: Google search functionality
     Given User is on Google home page
     Then User should see title is Google
 
-  @wip
+
   Scenario: User search title verification
     Given User is on Google home page
     When User searches for apple
