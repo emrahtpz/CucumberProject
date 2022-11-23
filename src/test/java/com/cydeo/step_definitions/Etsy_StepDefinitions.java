e6bc8b14a2cd637f61c07e7930795dfaad18bce9
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-23 13:13:31 +0300

    Implemented Etsy Step definitions

--- src/test/java/com/cydeo/step_definitions/Etsy_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Etsy_StepDefinitions.java
@@ -1,18 +1,28 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class Etsy_StepDefinitions {
 
     @Given("user is on the Etsy landing page")
     public void user_is_on_the_etsy_landing_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        String url = ConfigurationReader.getProperty("etsyUrl");
+
+        Driver.getDriver().get(url);
+
     }
     @Then("user should see Etsy title as expected")
     public void user_should_see_etsy_title_as_expected() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        String expectedTitle = "Etsy Turkey - Shop for handmade, vintage, custom, and unique gifts for everyone";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue("Title is not expected!", actualTitle.equals(expectedTitle));
+
     }
 }
